ac36dc7de0344ea2ed77464cf366840ae93cc216
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-12-13 16:46:51 -0600

    created questions

---
+++ README.md
@@ -0,0 +1,37 @@
+# EZ-README-Generator
+
+## Table of Contents
+* [Description](#description)
+* [Screenshot](#screenshot)
+* [Link](#link)
+* [Code](#code)
+* [Contact](#contact)
+* [Technologies](#technologies)
+* [Project Status](#project-status)
+
+## Description 
+This console-based application converts data entered by a user into a formatted README file.
+
+## Screenshot
+![Screenshot of portfolio webpage](./assets/images/readme-screenshot.png)
+
+
+## Link
+[Video Walkthrough](https://johny49.github.io/weather-dashboard/)
+
+
+## Code
+[GitHub Code Repository](https://github.com/Johny49/ez-readme)
+
+
+## Contact 
+Created by [@johny49](https://github.com/Johny49/) - feel free to contact me!
+
+
+## Technologies
+- JavaScript
+- Node
+- Inquirer.js
+
+## Project Status
+Project is: completed and functioning as intended.
--- index.js
+++ index.js
@@ -1,13 +1,116 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description:',
+    },
+    {
+        type: 'list',
+        name: 'provideUseInstructions',
+        message: 'Do you wish to provide instructions for using the project?',
+        choices: ['yes', 'no'],
+    },
+    {
+        type: 'input',
+        name: 'usageInstructions',
+        message: 'List your usage instructions:',
+        when: function (answers) {
+            //only show if provideInstructions is true
+            return answers.provideUseInstructions === 'yes';
+        },
+    },
+    {
+        type: 'list',
+        name: 'provideInstallInstructions',
+        message: 'Do you wish to provide instructions for installation?',
+        choices: ['yes', 'no'],
+    },
+    {
+        type: 'input',
+        name: 'installInstructions',
+        message: 'Provide any steps for installation:',
+        when: function (answers) {
+            //only show if provideInstructions is true
+            return answers.provideUseInstructions === 'yes';
+        },
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+    }, 
+    {
+        type: 'input',
+        name: 'emailAddress',
+        message: 'What is your email address?',
+    }, 
+    {
+        type: 'list',
+        name: 'provideContributionInstructions',
+        message: 'Do you wish to include steps on how users can contribute?',
+        choices: ['yes', 'no'],
+    },
+    {
+        type: 'input',
+        name: 'contributionInstructions',
+        message: 'Please provide instructions for user contribution:',
+        when: function (answers) {
+            //only show if provideInstructions is true
+            return answers.provideContributionInstructions === 'yes';
+        },
+    },
+    {
+        type: 'input',
+        name: 'repoLink',
+        message: 'What is the link for your GitHub repo?',
+    },
+    {
+        type: 'input',
+        name: 'deployedLink',
+        message: 'What is the link for your deployed application?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license option applies to your project?',
+        choices: ["Apache", "BSD", "GPL", "MIT"],
+    },
+    {
+        type: 'list',
+        name: 'provideTestInfo',
+        message: 'Do you have information about tests that you wish to include?',
+        choices: ['yes', 'no'],
+    },
+    {
+        type: 'input',
+        name: 'testInfo',
+        message: 'Provide information about tests for your project:',
+        when: function (answers) {
+            //only show if provideInstructions is true
+            return answers.provideTestInfo === 'yes';
+        },
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
-
+function init() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            console.log(answers)
+});
+}
 // Function call to initialize app
 init();
